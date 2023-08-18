3f208acbb23fda2b74d2b0104197cfbf9af9c1b0
noahspachinger <noahspachinger@gmail.com>
2023-08-18 15:23:58 +0200

    create all files and write down pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+//Object constructor that can create new book objects
+//Array that stores all of the book objects
+//Function that displays all the books on the page
+//New Book button that displays a form where the user can enter new books
+//Delete button for every book
+//Button that changes the read status
