688832d13138d74d5bd6cb10d0d8e875c2249bf3
noahspachinger <noahspachinger@gmail.com>
2023-08-28 10:40:23 +0200

    Design layout

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Object constructor that can create new book objects
+
 //Array that stores all of the book objects
 //Function that displays all the books on the page
 //New Book button that displays a form where the user can enter new books
 //Delete button for every book
 //Button that changes the read status
+
+// color palette: https://coolors.co/palette/e9f5db-cfe1b9-b5c99a-97a97c-87986a-718355
