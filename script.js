b8b1ce28bbf56e8273d064885c654137e5075091
noahspachinger <noahspachinger@gmail.com>
2023-11-19 14:31:31 +0100

    styling add book button and other stuff but i don't remember

--- script.js
+++ script.js
@@ -8,28 +8,50 @@ function Book(title, author, status) {
 //Array that stores all of the book objects
 let library = [];
 
-//Function that displays all the books on the page
-function displayBook(identifier, title, author, status) {
-    let currentBook = document.createElement("div");
-    currentBook.setAttribute("id", identifier);
-    currentBook.classList.add("books");
-    document.querySelector("#bookshelf").appendChild(currentBook);
-
-    let currentBookTitle = document.createElement("h2");
-    currentBookTitle.textContent = title;
-    currentBookTitle.classList.add("booktitle");
-    document.querySelector("#" + identifier).appendChild(currentBookTitle)
-
-    let currentBookAuthor = document.createElement("p");
-    currentBookAuthor.textContent = author;
-    currentBookAuthor.classList.add("bookauthor");
-    document.querySelector("#" + identifier).appendChild(currentBookAuthor);
-
-    let currentBookStatus = document.createElement("p");
-    currentBookStatus.textContent = status;
-    currentBookStatus.classList.add("bookstatus");
-    document.querySelector("#" + identifier).appendChild(currentBookStatus);
+//Function that adds Book Objects to the array
+function addBookToLibrary(bookObject) {
+    library.push(bookObject);
 }
+
+//Function that displays all the books on the page
+let bookNumber = "";
+function displayBook() {
+    let i = 0;
+
+    while (i < library.length) {
+
+        bookNumber = "book" + (i + 1);
+        
+        let currentBook = document.createElement("div");
+        currentBook.setAttribute("id", bookNumber);
+        currentBook.classList.add("books");
+        document.querySelector("#bookshelf").appendChild(currentBook);
+
+        let currentBookTitle = document.createElement("h2");
+        currentBookTitle.textContent = library[i].title;
+        currentBookTitle.classList.add("booktitle");
+        document.querySelector("#" + bookNumber).appendChild(currentBookTitle);
+
+        let currentBookAuthor = document.createElement("p");
+        currentBookAuthor.textContent = library[i].author;
+        currentBookAuthor.classList.add("bookauthor");
+        document.querySelector("#" + bookNumber).appendChild(currentBookAuthor);
+
+        let currentBookStatus = document.createElement("p");
+        currentBookStatus.textContent = library[i].status;
+        currentBookStatus.classList.add("bookstatus");
+        document.querySelector("#" + bookNumber).appendChild(currentBookStatus);
+
+        i++
+    }
+}
+
+let HP = new Book("Harry Potter", "JK Rowling", "read");
+addBookToLibrary(HP);
+let Bible = new Book("Bible", "Jesus", "not read yet");
+addBookToLibrary(Bible);
+
+
 //New Book button that displays a form where the user can enter new books
 //Delete button for every book
 //Button that changes the read status
